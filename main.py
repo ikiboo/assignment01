dd14351e72ab0accae69790e1c4233a99d916d17
ikiboo <dochovska.iskra@gmail.com>
2023-05-31 19:13:29 +0200

    commit 5

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from extras.desc import desc
 from swap import swap
 
-desc(5)
-swap(0,2)
+print("Please enter two numbers to swap to string:")
+num1 = int(input())
+num2 = int(input())
+print(swap(num1,num2))
+
+print("Please enter a number to print it in natural descending order:")
+n = int(input())
+desc(n)
